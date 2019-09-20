f9b23c9e013c8ff9e439a3c234307d8952d729c0
Amelie <amelieoller@gmail.com>
2019-09-20 10:02:36 -0700

    Create intro to inheritance lecture solution

---
+++ 05-intro-inheritance-oo-my-pets/lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog < Animal
+  
+end
+
+
+
+# class Dog < Animal
+  # attr_reader :name
+  # attr_accessor :mood
+  #
+  # def initialize(name)
+  #   @name = name
+  #   @mood = 'nervous'
+  # end
+# end
