e09b5b870d5fa1db933ef3756501f1fbbc1ea02c
Levi Miller <paullevimiller@gmail.com>
2019-09-30 11:48:55 -0700

    Complete lecture

--- 11-hashes-and-the-internet/app.rb
+++ 11-hashes-and-the-internet/app.rb
@@ -2,8 +2,52 @@ require 'json'
 require 'pry'
 require 'rest-client'
 
-response = RestClient.get('http://www.wikipedia.org')
+puts
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+puts "} Welcome to the BookWorm CLI! {"
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+puts
+puts "Enter a title or some keywords for books in which you're interested."
+user_input = gets.chomp
+puts 
 
-binding.pry
+user_input_formatted = user_input.split(" ").join("+")
 
-puts "Getting stuff!"
+url = "https://www.googleapis.com/books/v1/volumes?q=#{user_input_formatted}"
+response = RestClient.get(url)
+json = JSON.parse(response)
+
+all_info_books = json["items"].map do |book|
+    book["volumeInfo"]
+end
+
+some_info_books = all_info_books.map do |book|
+    title = book["title"]
+    authors = book["authors"]
+    description = book["description"]
+    {
+        :title => title,
+        :authors => authors, 
+        :description => description
+    } 
+end
+
+# json["items"][0]["volumeInfo"]
+octothorpe_wall = "#" * 79
+
+some_info_books.each do |book|
+    puts octothorpe_wall
+    puts
+    puts book[:title]
+    puts book[:authors]
+    print "*-" * 39
+    puts "*"
+    puts book[:description]
+    puts
+end
+
+puts octothorpe_wall
+
+# binding.pry
+
+# puts "Getting stuff!"
