c03da63cd9706f387017f77274af9c5441166653
Levi Miller <paullevimiller@gmail.com>
2019-10-16 14:06:08 -0700

    Removed Lecture 21

--- 21-rails-forms-validations/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :aliens
-  get "/search", to: "aliens#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
